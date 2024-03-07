f9e39a6d5fba43a12a74ac320a723ac8957baa48
rndm <rndm@localhost.localdomain>
2024-03-07 19:23:24 +0000

    feat: Added ssl to httplib support

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # A rewrite of a my web testing gui python app in C++
+
+## Installation:
+```
+$ todo
+```
+
+## Uses:
+- [cpp-httplib](https://github.com/yhirose/cpp-httplib) 
+- [nlohmann/json](https://github.com/nlohmann/json)
+- [imgui_bundle](https://github.com/pthom/imgui_bundle) 
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,64 @@
 #include "immapp/immapp.h"
 #include "imgui_md_wrapper.h"
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "../external/cpp-httplib/httplib.h"
+#include "../external/json-include/json.hpp"
 
 struct AppState {
-
+    httplib::Client cli;
 };
 
 void tests(AppState* state) noexcept {
-
+    ImGui::Text("Hello, this is tests window!");
 }
 
-std::vector<HelloImGui::DockingSplit> splits() noexcept { return {}; }
+std::vector<HelloImGui::DockingSplit> splits() noexcept { 
+    return {};
+}
 
 std::vector<HelloImGui::DockableWindow> windows(AppState *state) noexcept {
-  auto customers_window = HelloImGui::DockableWindow(
-      "Customers", "MainDockSpace", [state]() { tests(state); });
+    auto customers_window = HelloImGui::DockableWindow(
+            "Tests", "MainDockSpace", [state]() { tests(state); });
 
+    auto logs_window = HelloImGui::DockableWindow(
+            "Logs", "MainDockSpace", [state]() { HelloImGui::LogGui(); });
 
-  auto logs_window = HelloImGui::DockableWindow(
-      "Logs", "MainDockSpace", [state]() { HelloImGui::LogGui(); });
-
-  return {customers_window, logs_window};
+    return {customers_window, logs_window};
 }
 
 HelloImGui::DockingParams layout(AppState *state) noexcept {
-  auto params = HelloImGui::DockingParams();
+    auto params = HelloImGui::DockingParams();
 
-  params.dockableWindows = windows(state);
-  params.dockingSplits = splits();
+    params.dockableWindows = windows(state);
+    params.dockingSplits = splits();
 
-  return params;
+    return params;
 }
 
 void show_gui(AppState *state) noexcept {
-  auto io = ImGui::GetIO();
+    auto io = ImGui::GetIO();
 }
 
 int main(int argc, char *argv[]) {
-  // httplib::Client cli("http://192.168.213.67:8000");
-  auto state = AppState{
-      // .cli = std::move(cli),
-  };
+    httplib::Client cli("");
+    auto state = AppState{
+        .cli = std::move(cli),
+    };
 
-  HelloImGui::RunnerParams runner_params;
-  runner_params.appWindowParams.windowTitle = "weetee";
-  runner_params.imGuiWindowParams.defaultImGuiWindowType =
-      HelloImGui::DefaultImGuiWindowType::ProvideFullScreenDockSpace;
-  runner_params.callbacks.ShowGui = [&state]() { show_gui(&state); };
+    HelloImGui::RunnerParams runner_params;
+    runner_params.appWindowParams.windowTitle = "weetee";
 
-  runner_params.dockingParams = layout(&state);
+    runner_params.imGuiWindowParams.showMenuBar = true;
+    runner_params.imGuiWindowParams.showStatusBar = true;
+    runner_params.imGuiWindowParams.defaultImGuiWindowType =
+        HelloImGui::DefaultImGuiWindowType::ProvideFullScreenDockSpace;
+    runner_params.callbacks.ShowGui = [&state]() { show_gui(&state); };
 
-  ImmApp::AddOnsParams addOnsParams;
-  addOnsParams.withMarkdown = true;
-  ImmApp::Run(runner_params, addOnsParams);
-  return 0;
+    runner_params.dockingParams = layout(&state);
+
+    ImmApp::AddOnsParams addOnsParams;
+    addOnsParams.withMarkdown = true;
+    ImmApp::Run(runner_params, addOnsParams);
+    return 0;
 }
